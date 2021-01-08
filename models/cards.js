63eb28319c03f571cb4b532d87800b89bac5631b
Christopher Fuentes <fuenteschris75@gmail.com>
2021-01-08 14:02:30 -0600

    Adding icon to cards

--- models/cards.js
+++ models/cards.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const cardSchema = mongoose.Schema({
     title:String,
     description:String,
-    descriptionForm:String
+    descriptionForm:String,
+    icon:String
 });
 module.exports = mongoose.model('Card',cardSchema);
